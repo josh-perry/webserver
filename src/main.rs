68bb6b5d50b8e47b75c3654820248f5ca6bd2aea
Josh Perry <joshperry@outlook.com>
2019-07-20 12:57:53 +0100

    Populate more of the request struct + debug printing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
 use std::str;
 use std::path::Path;
 use std::fs;
+use std::fmt;
 use std::io::prelude::*;
 use std::collections::HashMap;
 use std::net::{TcpListener, TcpStream, Shutdown};
 
 const BUFFER_SIZE: usize = 1024;
 
+#[derive(Debug)]
+enum Verb {
+    GET,
+    //HEAD,
+    //POST,
+    //PUT,
+    //DELETE,
+    //CONNECT,
+    //OPTIONS,
+    //TRACE,
+    //PATCH
+}
+
+#[derive(Debug)]
 struct Request {
+    verb: Verb,
+    path: String,
     body: String,
     headers: HashMap<String, String>
+}
+
+impl fmt::Display for Request {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:#?}", self)
+    }
 }
 
 fn get_request(stream: &mut TcpStream) -> Request {
@@ -30,9 +53,20 @@ fn get_request(stream: &mut TcpStream) -> Request {
 
     let request = str::from_utf8(&buf).unwrap();
     let mut headers = HashMap::new();
+    let request_lines = request.lines();
 
-    for line in request.lines() {
+    let mut verb = Verb::GET;
+    let mut path = "".to_string();
+
+    for line in request_lines {
         if !line.contains(":") {
+            if line.starts_with("GET") {
+                let split_line: Vec<&str> = line.splitn(3, " ").collect();
+
+                verb = Verb::GET;
+                path = split_line[1].trim().to_string();
+            }
+
             continue;
         }
 
@@ -41,13 +75,16 @@ fn get_request(stream: &mut TcpStream) -> Request {
     }
 
     Request {
-        body: request.to_string(),
-        headers: headers
+        body: request.trim_matches(char::from(0)).to_string(),
+        headers: headers,
+        verb: verb,
+        path: path
     }
 }
 
 fn handle_client(mut stream: TcpStream) {
     let request = get_request(&mut stream);
+    println!("{}", request.to_string());
 
     let header = String::from("HTTP/1.1 200 OK");
 
